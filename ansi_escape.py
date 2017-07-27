83e1b53a971a78b0e30fe29de34ef8f0dfb55576
Bas Pennings <baspster@gmail.com>
2017-07-27 22:03:32 +0200

    LambdaMOO docs

--- ansi_escape.py
+++ ansi_escape.py
@@ -1,3 +1,6 @@
+# I got sick of trying to find out how to *get* the ANSI ESC character.
+# This script will write a text file that contains the ANSI ESC character.
+# Have fun copy pasting it into your MOO database! :P
 f = open("ansi_escape.txt", "w");
 f.write(u"\u001b");
 f.close();
